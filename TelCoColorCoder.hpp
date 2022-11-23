d41bae9808558a6b2ce1339a5ed965563b8de950
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-11-23 13:28:40 +0530

    Update TelCoColorCoder.hpp

--- TelCoColorCoder.hpp
+++ TelCoColorCoder.hpp
@@ -8,6 +8,9 @@
 namespace TelCoColorCoder
 {
 
+  enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+  enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
+  
   class ColorPair
   {
   private:

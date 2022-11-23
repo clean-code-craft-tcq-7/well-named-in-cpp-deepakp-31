64dbc5525e2209f4281ad78e4e27abe5c1097ae0
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-11-23 11:22:30 +0530

    Update TelCoColorCoder.hpp
    Added enum variables

--- TelCoColorCoder.hpp
+++ TelCoColorCoder.hpp
@@ -1,28 +1,47 @@
-#ifndef TelCoColorCoder_HPP
-#define TelCoColorCoder_HPP
+#ifndef TEL_COCOLOR_CODER_HPP
+#define TEL_COCOLOR_CODER_HPP
 
 #include <iostream>
 #include <assert.h>
 
 namespace TelCoColorCoder
 {
-  class ColorPair {
-        private:
-            MajorColor majorColor;
-            MinorColor minorColor;
-        public:
-            ColorPair(MajorColor major, MinorColor minor):
-                majorColor(major), minorColor(minor)
-            {}
-    
-            MajorColor getMajor() ;
-            MinorColor getMinor() ;
-            std::string ToString() ;
-    };
-  
-  ColorPair GetColorFromPairNumber(int pairNumber) ;
-    int GetPairNumberFromColor(MajorColor major, MinorColor minor) ;
-  
+  enum MajorColor
+  {
+    WHITE,
+    RED,
+    BLACK,
+    YELLOW,
+    VIOLET
+  };
+  enum MinorColor
+  {
+    BLUE,
+    ORANGE,
+    GREEN,
+    BROWN,
+    SLATE
+  };
+
+  class ColorPair
+  {
+  private:
+    MajorColor majorColor;
+    MinorColor minorColor;
+
+  public:
+    ColorPair(MajorColor major, MinorColor minor) : majorColor(major), minorColor(minor)
+    {
+    }
+
+    MajorColor getMajor();
+    MinorColor getMinor();
+    std::string ToString();
+  };
+
+  ColorPair GetColorFromPairNumber(int pairNumber);
+  int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+
 }
 
-#endif 
+#endif

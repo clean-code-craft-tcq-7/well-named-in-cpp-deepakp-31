8688c2fe254555e251a03f19530a11288ef10abf
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-11-23 11:29:11 +0530

    Create TelCoColorCoder_main.cpp
    added driver function

---
+++ TelCoColorCoder_main.cpp
@@ -0,0 +1,11 @@
+#inclde "TelCoColorCoder.cpp"
+
+int main() {
+    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
+    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
+
+    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
+    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+
+    return 0;
+}

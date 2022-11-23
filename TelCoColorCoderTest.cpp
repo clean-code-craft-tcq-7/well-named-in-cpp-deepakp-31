23df62937aec6c61c745a204d12d4fff39636ea9
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-11-23 14:58:06 +0530

    Create TelCoColorCoderTest.cpp
    Checking to separate test . creating cpp test file

---
+++ TelCoColorCoderTest.cpp
@@ -0,0 +1,27 @@
+#ifndef TEL_COCOLOR_CODER_HPP
+#define TEL_COCOLOR_CODER_HPP
+
+#include "TelCoColorCoderTest.hpp"
+
+void testNumberToPaircheck(int pairNumber,
+                      TelCoColorCoder::MajorColor expectedMajor,
+                      TelCoColorCoder::MinorColor expectedMinor)
+{
+    TelCoColorCoder::ColorPair colorPair =
+        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+void testPairToNumbercheck(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber)
+{
+    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
+}
+
+#endif

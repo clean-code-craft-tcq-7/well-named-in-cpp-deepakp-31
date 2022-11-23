3cfa97f5ed1dc369f8f57aa9c76b4eb99945df52
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-11-23 12:06:03 +0530

    Create TelCoColorCoderTest.hpp
    TelCoColorCoderTest hpp file

---
+++ TelCoColorCoderTest.hpp
@@ -0,0 +1,16 @@
+#ifndef TEL_COCOLOR_CODER_TEST_HPP
+#define TEL_COCOLOR_CODER_TEST_HPP
+#inclde "TelCoColorCoder.hpp"
+
+void testNumberToPair(int pairNumber,
+                                       TelCoColorCoder::MajorColor expectedMajor,
+                                       TelCoColorCoder::MinorColor expectedMinor);
+
+
+void testPairToNumber(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber);
+
+
+#endif

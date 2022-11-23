28bd27b0b0cac169842327e54e2243d08cada4e8
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-11-23 14:39:12 +0530

    Update TelCoColorCoder.hpp

--- TelCoColorCoder.hpp
+++ TelCoColorCoder.hpp
@@ -31,8 +31,7 @@ namespace TelCoColorCoder
   };
 
   ColorPair GetColorFromPairNumber(int pairNumber);
-  int GetPairNumberFromColor(MajorColor major, MinorColor minor);
-  
+  int GetPairNumberFromColor(MajorColor major, MinorColor minor);  
   void printWiringManual();
 
 }
